89560ba3908a03b661885a34b15f4ce725fa4635
jangalacharansaisri963-eng <jangalacharansaisri963@gmail.com>
2026-08-29 18:08:23 +0530

    Update dna.js

--- audio/dna.js
+++ audio/dna.js
@@ -1,16 +1,33 @@
 registerAnimationAudio("dna", () => {
-playTone(300, 0.12, "sine", 0.025);
+    // Deep activation thump
+    playTone(95, 0.28, "sine", 0.035);
 
-setTimeout(() => {
-    playTone(450, 0.12, "sine", 0.025);
-}, 100);
+    // Metallic molecular tick
+    setTimeout(() => {
+        playTone(1250, 0.045, "square", 0.012);
+    }, 80);
 
-setTimeout(() => {
-    playTone(600, 0.15, "sine", 0.02);
-}, 200);
+    // Resonant scan
+    setTimeout(() => {
+        playTone(530, 0.35, "triangle", 0.018);
+    }, 150);
 
-setTimeout(() => {
-    playTone(750, 0.18, "triangle", 0.015);
-}, 320);
+    // Second scan layer
+    setTimeout(() => {
+        playTone(735, 0.28, "sine", 0.014);
+    }, 230);
 
+    // Tiny molecular clicks
+    setTimeout(() => {
+        playTone(1480, 0.035, "square", 0.009);
+    }, 310);
+
+    setTimeout(() => {
+        playTone(1720, 0.035, "square", 0.007);
+    }, 365);
+
+    // Deep resonant finish
+    setTimeout(() => {
+        playTone(210, 0.5, "sine", 0.016);
+    }, 430);
 });
