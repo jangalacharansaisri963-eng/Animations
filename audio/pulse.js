8f02d29c8c9334f057618a718c6b859b955b3b9a
jangalacharansaisri963-eng <jangalacharansaisri963@gmail.com>
2026-08-29 18:25:42 +0530

    Update pulse.js

--- audio/pulse.js
+++ audio/pulse.js
@@ -1,12 +1,36 @@
 registerAnimationAudio("pulse", () => {
-playTone(110, 0.3, "sine", 0.05);
+    // Deep reactor ignition
+    playTone(55, 0.45, "sine", 0.055);
 
-setTimeout(() => {
-    playTone(165, 0.25, "sine", 0.035);
-}, 100);
+    // Rising power
+    setTimeout(() => {
+        playTone(82.41, 0.30, "triangle", 0.040);
+    }, 90);
 
-setTimeout(() => {
-    playTone(220, 0.2, "sine", 0.02);
-}, 220);
+    setTimeout(() => {
+        playTone(123.47, 0.25, "triangle", 0.035);
+    }, 180);
 
+    // Energy buildup
+    setTimeout(() => {
+        playTone(185, 0.18, "sawtooth", 0.018);
+    }, 280);
+
+    setTimeout(() => {
+        playTone(277.18, 0.14, "square", 0.012);
+    }, 360);
+
+    // Reactor discharge
+    setTimeout(() => {
+        playTone(554.37, 0.06, "square", 0.025);
+    }, 440);
+
+    setTimeout(() => {
+        playTone(830.61, 0.055, "triangle", 0.018);
+    }, 475);
+
+    // Deep energy impact
+    setTimeout(() => {
+        playTone(65.41, 0.55, "sine", 0.035);
+    }, 520);
 });
